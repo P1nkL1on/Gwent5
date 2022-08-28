4fcac618cec024dec201a4d110fe8db6108335c7
Leonid <lion6149@mail.ru>
2022-08-29 01:24:24 +0300

    Add autoresolving into the test_choice

--- Tests/Cases/test_choice.cpp
+++ Tests/Cases/test_choice.cpp
@@ -32,12 +32,14 @@ int main()
     ally.cardStack2.isRemovingExpandedChoice = false;
     geralt->onDeploy(ally, enemy);
     geralt->onDeploy(ally, enemy);
+    ally.cardStack2.expandNextChoiceAndTryResolveIt();
 
     // First choice autoexpanded, but not autoresolved (due to settings)
     testcase.add(int(ally.cardStack2.stack()[0].screenRows.size()), 1, "First Igni should contain 1 option, expanded");
     testcase.add(int(ally.cardStack2.stack()[1].screenRows.size()), 3, "Second Igni should contain 3 options, unexpanded");
     testcase.add(int(ally.cardStack2.stack().size()), 2, "Choice stack should contain 2 choices");
     onChoiceDoneRow(4, ally, enemy);
+    ally.cardStack2.expandNextChoiceAndTryResolveIt();
     testcase.add(int(ally.cardStack2.stack().size()), 0, "Second Igni should be autoresolved anyway, "
                                                          "cause has no vaible options after expand");
     testcase.add(isIn(griffin, enemy.discard), 1, "Grifin should be destroyed");
@@ -47,10 +49,12 @@ int main()
 
     testcase.add(ally.cardStack2.isEmpty(), 1, "Choice stack should be empty");
     runestone->onPlaySpecial(ally, enemy);
+    ally.cardStack2.expandNextChoiceAndTryResolveIt();
     testcase.add(ally.cardStack2.isEmpty(), 0, "Choice stack shouldn't be empty");
-    testcase.add(int(ally.cardStack2.peek().options.size()), 3, "Create should has 3 cards");
-    onChoiceDoneCard(ally.cardStack2.peek().options[0], ally, enemy);
-    testcase.add(ally.cardStack2.isEmpty(), 0, "Choice stack shouldn't be empty still");
+    if (testcase.add(int(ally.cardStack2.peek().options.size()), 3, "Create should has 3 cards")) {
+        onChoiceDoneCard(ally.cardStack2.peek().options[0], ally, enemy);
+        testcase.add(ally.cardStack2.isEmpty(), 0, "Choice stack shouldn't be empty still");
+    }
 
     return testcase.process();
 }
